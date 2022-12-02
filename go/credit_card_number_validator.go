1cdfc86f0a9ab08b9d79040df2e73dc6c9f551d3
Martin Sidorov <martin.sidorov@wallester.com>
2022-12-02 17:10:13 +0200

    Add credit card number validator project

--- go/credit_card_number_validator.go
+++ go/credit_card_number_validator.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	var creditCardNumber string
+	flag.StringVar(&creditCardNumber, "credit_card_number", "0", "Credit card number")
+	flag.Parse()
+
+	validateCreditCardNumber(creditCardNumber)
+
+	fmt.Println("Entered credit card number:", creditCardNumber)
+
+	if checkLuhnAlgorithm(creditCardNumber) {
+		fmt.Println("Credit card number is valid")
+	} else {
+		fmt.Println("Credit card number is invalid")
+	}
+}
+
+func validateCreditCardNumber(creditCardNumber string) string {
+	if creditCardNumber == "" {
+		fmt.Println("Please provide a credit card number by running the program with the -credit_card_number flag")
+	}
+
+	if len(creditCardNumber) != 16 {
+		fmt.Println("Credit card number must be 16 digits")
+	}
+
+	return ""
+}
+
+func checkLuhnAlgorithm(creditCardNumber string) bool {
+	var sum int
+	for i, digit := range creditCardNumber {
+		if i%2 == 0 {
+			sum += int(digit - '0')
+		} else {
+			sum += 2 * int(digit-'0')
+		}
+	}
+
+	return sum%10 == 0
 }
